4cafac23007d1b2fe05489fcb83c627b038fb20d
TonyKaung <kaungkxant56@gmail.com>
2026-05-19 14:04:23 -0700

    Week 1, deep.py

---
+++ Week_1/deep.py
@@ -0,0 +1,5 @@
+u_input = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if u_input == "42" or u_input.lower() == "forty-two" or u_input.lower() == "forty two":
+    print("Yes")
+else:
+    print("No")
