94e77223644f04755b391cfcfbc32f12afedc4b6
TonyKaung <kaungkxant56@gmail.com>
2026-05-19 14:41:14 -0700

    Week 1, interpreter.py

---
+++ Week_1/interpreter.py
@@ -0,0 +1,14 @@
+expression = input("Expression: ")
+x, y, z = expression.split(" ")
+x = int(x)
+z = int(z)
+if(y == "+"):
+    print(f"{x + z:.1f}")
+elif(y == "-"):
+    print(f"{x - z:.1f}")
+elif(y == "*"):
+    print(f"{x * z:.1f}")
+elif(y == "/" and z != 0):
+    print(f"{x / z:.1f}")
+else:
+    print("Invalid expression")
