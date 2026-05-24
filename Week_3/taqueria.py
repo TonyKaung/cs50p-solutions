a2b6888ac115c95f2bdcc18e92300259a47f91f8
TonyKaung <kaungkxant56@gmail.com>
2026-05-24 01:24:09 -0700

    Week 3, taqueria.py

---
+++ Week_3/taqueria.py
@@ -0,0 +1,28 @@
+def main():
+    d = {
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla": 2.00}
+    for i in d:
+        print(f"{i}: ${d[i]:.2f}")
+    print("\n")
+    menu(d)
+        
+def menu(d):
+    total = 0
+    while True:
+        try:
+            order = input("Item: ").title()
+            if order in d:
+                total += d.get(order)
+                print(f"Total: ${total:.2f}")
+        except EOFError:
+            break
+
+main()
