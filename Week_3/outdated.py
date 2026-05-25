25bdb6cd4aee870cbf08ae3fed9e29859d54294e
TonyKaung <kaungkxant56@gmail.com>
2026-05-24 20:56:50 -0700

    Week 3, outdated.py

--- Week_3/outdated.py
+++ Week_3/outdated.py
@@ -1,29 +1,21 @@
+months = ["january", "february", "march", "april", "may", "june",
+          "july", "august", "september", "october", "november", "december"]
+
 while True:
-    dates = input("Date: ").lower().strip()
-    if "/" in dates:
-        try:
+    dates = input("Date: ").strip().lower()
+    try:
+        if "/" in dates:
             month, day, year = dates.split("/")
-            month = int(month)
-            day = int(day) 
-            year = int(year)
-            if not (1<= month <= 12 and 1 <= day <= 31 and year >= 0):
-                continue
-            print(f"{year:04d}-{month:02d}-{day:02d}")
-            break
-        except ValueError:
-            continue
-    else:
-        try:
+            month, day, year = int(month), int(day), int(year)
+        else:
             month_day, year = dates.split(", ")
             month, day = month_day.split(" ")
-            month = month.strip(",").lower()
-            day = int(day)
-            year = int(year)
-            months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-            if month not in months or not (1 <= day <= 31 and year >= 0):
+            day, year = int(day), int(year)
+            if month not in months:
                 continue
             month = months.index(month) + 1
+        if 1 <= month <= 12 and 1 <= day <= 31 and year >= 0:
             print(f"{year:04d}-{month:02d}-{day:02d}")
             break
-        except ValueError:
-            continue
+    except ValueError:
+        continue
