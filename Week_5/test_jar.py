b63ffdf4fec4f8ad274ae5385a7a25c1f6229f46
TonyKaung <kaungkxant56@gmail.com>
2026-06-07 22:08:47 -0700

    Problem set 8, test_jar.py

---
+++ Week_5/test_jar.py
@@ -0,0 +1,33 @@
+from jar import Jar
+
+
+def test_init():
+    jar = Jar()
+    assert jar.capacity == 12
+    assert jar.size == 0
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(5)
+    assert jar.size == 5
+    jar.deposit(7)
+    assert jar.size == 12
+
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(10)
+    jar.withdraw(3)
+    assert jar.size == 7
+    jar.withdraw(7)
+    assert jar.size == 0
