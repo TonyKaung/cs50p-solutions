8f1b6c4c822a74db18efb7019410dba0ccd3568d
TonyKaung <kaungkxant56@gmail.com>
2026-06-18 20:51:38 -0700

    feat: Regular Expressions, numb3rs.py

--- Week_7/numb3rs.py
+++ Week_7/numb3rs.py
@@ -5,9 +5,11 @@ def main():
 
 
 def validate(ip):
+    # check the IP address format
     if re.search(r"^(\d{1,3}\.){3}\d{1,3}$", ip):
         numbers = ip.split(".")
         for number in numbers:
+            # check the validity of the number range
             if int(number) < 0 or int(number) > 255:
                 return False
         return True
