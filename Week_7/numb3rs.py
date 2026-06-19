8742fbc45bde595ff05423b08734630e1c49c26c
TonyKaung <kaungkxant56@gmail.com>
2026-06-18 20:46:34 -0700

    feat: Regular Expressions, numb3rs.py

---
+++ Week_7/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip):
+    if re.search(r"^(\d{1,3}\.){3}\d{1,3}$", ip):
+        numbers = ip.split(".")
+        for number in numbers:
+            if int(number) < 0 or int(number) > 255:
+                return False
+        return True
+
+if __name__ == "__main__":
+    main()
